d469ccb88070ebcb0c53ae66edc979fd542c18c6
Zack Walsh <zachary.walsh@opusivs.com>
2022-01-11 16:01:06 -0500

    Placeholder change and Zip fix
    Changed placeholder text. Updated zip code to stop crashing

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("2.16.23.1006")]
-[assembly: AssemblyFileVersion("2.16.23.1006")]
+[assembly: AssemblyVersion("2.16.23.1007")]
+[assembly: AssemblyFileVersion("2.16.23.1007")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
