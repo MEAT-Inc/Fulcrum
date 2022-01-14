f55bc8d065135d6518ab4bc59b1dfb22c15ca6f8
Zack Walsh <zachary.walsh@opusivs.com>
2022-01-14 13:35:43 -0500

    Building for hotfix to installer
    Fixing for release of installer app

---
+++ FulcrumInjector/FulcrumLogic/PassThruRegex/PtRegexResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulcrumInjector.FulcrumLogic.PassThruRegex
+{
+    /// <summary>
+    /// Class object containing the output results of a PT Regex filtering run for a particular line.
+    /// These are used to trigger conditions in the logging filter helpers.
+    /// </summary>
+    public class PtRegexResult
+    {
+
+    }
+}
