d3719e4ea01a07bd5deaca5248399152f8dbf896
Zack Walsh <zachary.walsh@opusivs.com>
2022-01-17 13:25:30 -0500

    Built loading operations for log files.
    Loading input files now done.  Added information view content to Debug Logging output view and built in small style changes for those views

--- FulcrumInjector/FulcrumViewContent/ViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
+++ FulcrumInjector/FulcrumViewContent/ViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FulcrumInjector.FulcrumViewContent.Views.InjectorCoreViews;
 using FulcrumInjector.FulcrumViewSupport.AppStyleSupport.AvalonEditHelpers;
 using SharpLogger;
 using SharpLogger.LoggerObjects;
@@ -20,10 +22,12 @@ namespace FulcrumInjector.FulcrumViewContent.ViewModels.InjectorCoreViewModels
             .FirstOrDefault(LoggerObj => LoggerObj.LoggerName.StartsWith("InjectorLogReviewViewModelLogger")) ?? new SubServiceLogger("InjectorLogReviewViewModelLogger");
 
         // Private control values
-        private string _loadedLogFile;
+        private string _loadedLogFile = "";
+        private string _logFileContents;
 
         // Public values for our view to bind onto 
         public string LoadedLogFile { get => _loadedLogFile; set => PropertyUpdated(value); }
+        public string LogFileContents { get => _logFileContents; set => PropertyUpdated(value); }
 
         // --------------------------------------------------------------------------------------------------------------------------
 
@@ -42,5 +46,48 @@ namespace FulcrumInjector.FulcrumViewContent.ViewModels.InjectorCoreViewModels
         }
 
         // --------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Loads the contents of an input log file object from a given path and stores them into the view.
+        /// </summary>
+        /// <param name="InputLogFile"></param>
+        public void LoadLogFileContents(string InputLogFile)
+        {
+            // Log information, load contents, store values.
+            ViewModelLogger.WriteLog("LOADING NEW LOG FILE CONTENTS NOW...", LogType.InfoLog);
+            FulcrumLogReviewView CastView = this.BaseViewControl as FulcrumLogReviewView;
+
+            try
+            {
+                // Log passed and return output.
+                this.LoadedLogFile = InputLogFile;
+                this.LogFileContents = File.ReadAllText(InputLogFile);
+
+                // Store lines here.
+                CastView.Dispatcher.Invoke(() =>
+                {
+                    CastView.LoadedLogFileTextBox.Text = this.LoadedLogFile;
+                    CastView.ReplayLogInputContent.Text = this.LogFileContents;
+                });
+
+                // Log complete and return.
+                ViewModelLogger.WriteLog($"LOADED CONTENTS OF LOG FILE {InputLogFile} CORRECTLY AND STORED ONTO VIEW INSTANCE!", LogType.InfoLog);
+            }
+            catch (Exception Ex)
+            {
+                // Log failed to load and set our contents to just "Failed to Load!" with the exception stack trace.
+                ViewModelLogger.WriteLog("FAILED TO LOAD NEW LOG FILE! VIEW IS SHOWING STACK TRACE NOW!", LogType.InfoLog);
+                ViewModelLogger.WriteLog("EXCEPTION THROWN IS BEING LOGGED BELOW.", Ex);
+
+                // Store new values.
+                this.LoadedLogFile = $"Failed to Load File: {Path.GetFileName(InputLogFile)}!";
+                this.LogFileContents = Ex.Message + "\n" + "STACK TRACE:\n" + Ex.StackTrace;
+                CastView.Dispatcher.Invoke(() =>
+                {
+                    CastView.LoadedLogFileTextBox.Text = this.LoadedLogFile;
+                    CastView.ReplayLogInputContent.Text = this.LogFileContents;
+                });
+            }
+        }
     }
 }
