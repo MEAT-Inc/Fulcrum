5a0d8fcccf99f0c03728a8b38cfc5f85a0eba0ba
Zack Walsh <zachary.walsh@opusivs.com>
2022-02-18 16:09:01 -0500

    Built in notification event helper
    Now able to process new device information being sent into the system from WinApis and into our main app process. This will then be used to connect to a JBox.

---
+++ FulcrumInjector/FulcrumLogic/PassThruWatchdog/DeviceStateCallbackTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulcrumInjector.FulcrumLogic.PassThruWatchdog
+{
+    /// <summary>
+    /// Builds a new instance of our event watchdog handler for events on our device manager
+    /// This class contains two methods. One for device registered and one for removed.
+    /// They are only executed when a J2534 interface is connected to our machine.
+    /// We only will consume a MAX of 2 cardaq interfaces. If two are connected, then open one and force it to stay allocated by this app
+    /// If connected, make a SharpSession and pull Voltage values out and read a VIN. Then once done, setup a background refresh for voltage
+    /// For disconnected, set that the device is gone. Use events to fire off a JBoxLost Event that will modify our UI and reset the search routines
+    /// </summary>
+    public class DeviceStateCallbackTriggers
+    {
+
+    }
+}
--- FulcrumInjector/InjectorMainWindow.xaml.cs
+++ FulcrumInjector/InjectorMainWindow.xaml.cs
@@ -10,10 +10,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using FulcrumInjector.FulcrumLogic.InjectorPipes;
+using FulcrumInjector.FulcrumLogic.PassThruWatchdog;
 using FulcrumInjector.FulcrumViewContent;
 using FulcrumInjector.FulcrumViewContent.Models;
 using FulcrumInjector.FulcrumViewSupport;
@@ -91,5 +93,33 @@ namespace FulcrumInjector
             // Now call the routine in the constants file.
             InjectorConstants.ProcessAppExit(null, null);
         }
+
+        // --------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Processes a new device state change event on this main window 
+        /// </summary>
+        /// <param name="DeviceStateArgs">Event args for our device state change</param>
+        protected override void OnSourceInitialized(EventArgs DeviceStateArgs)
+        {
+            // Base override for source configuration
+            base.OnSourceInitialized(DeviceStateArgs);
+            InjectorMainLogger.WriteLog("PROCESSING A NEW SOURCE INIT EVENT TRIGGER NOW...", LogType.InfoLog);
+
+            // Adds the windows message processing hook and registers USB device add/removal notification.
+            HwndSource HandleSource = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (HandleSource == null) {
+                InjectorMainLogger.WriteLog("WARNING! MAIN SOURCE HANDLE WAS SEEN TO BE NULL!", LogType.ErrorLog);
+                return;
+            }
+
+            // Now build new handle object values and register our event
+            HandleSource.AddHook(DeviceManagerEventWatchdog.WindowEventHandlerHook);
+            DeviceManagerEventWatchdog.RegisterDeviceNotification(HandleSource.Handle);
+            InjectorMainLogger.WriteLog("REGISTERED NEW EVENT MONITORING TRIGGER CORRECTLY!", LogType.InfoLog);
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------------
+
     }
 }
