e315caad5b597e609b47b4f2349851b46e62646b
Zack Walsh <zachary.walsh@opusivs.com>
2022-02-18 16:10:46 -0500

    Included device manager query class
    Built class for querying device manager to find new connections

---
+++ FulcrumInjector/FulcrumLogic/PassThruWatchdog/CheckDeviceManagerVciList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulcrumInjector.FulcrumLogic.PassThruWatchdog
+{
+    /// <summary>
+    /// This class starts up a background refreshing routine that checks our Device Manager and returns out a J2534 interface
+    /// if one is currently on our system. This will contain a list of currently connected devices and will be modified on the connect/disconnect
+    /// events setup
+    /// </summary>
+    public class CheckDeviceManagerVciList
+    {
+    }
+}
