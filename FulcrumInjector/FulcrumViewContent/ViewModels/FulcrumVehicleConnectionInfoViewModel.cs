7464c0d8ba85f171ced86ba12451bd22702d7a6e
Zack Walsh <zachary.walsh@opusivs.com>
2022-03-07 13:42:56 -0500

    Designed UI for voltage and VIN
    VIN And Voltage View are now complete. Woking in remaining IOCTL Commands now.

--- FulcrumInjector/FulcrumViewContent/ViewModels/FulcrumVehicleConnectionInfoViewModel.cs
+++ FulcrumInjector/FulcrumViewContent/ViewModels/FulcrumVehicleConnectionInfoViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Linq;
 using FulcrumInjector.FulcrumLogic.PassThruWatchdog;
+using FulcrumInjector.FulcrumViewContent.Models.EventModels;
 using SharpLogger;
 using SharpLogger.LoggerObjects;
 using SharpLogger.LoggerSupport;
@@ -17,7 +19,29 @@ namespace FulcrumInjector.FulcrumViewContent.ViewModels
     {
         // Logger object.
         private static SubServiceLogger ViewModelLogger => (SubServiceLogger)LogBroker.LoggerQueue.GetLoggers(LoggerActions.SubServiceLogger)
-            .FirstOrDefault(LoggerObj => LoggerObj.LoggerName.StartsWith("InstalledHardwareViewModelLogger")) ?? new SubServiceLogger("InstalledHardwareViewModelLogger");
+            .FirstOrDefault(LoggerObj => LoggerObj.LoggerName.StartsWith("VehicleConnectionViewModelLogger")) ?? new SubServiceLogger("VehicleConnectionViewModelLogger");
+
+        // --------------------------------------------------------------------------------------------------------------------------
+
+        // Private control values
+        private string _selectedDevice;        // Name of the currently connected and consumed Device
+        private double _deviceVoltage;         // Last known voltage value. If no device found, this returns 0.00
+        private string _vehicleVIN;            // VIN Of the current vehicle connected
+        private string _vehicleInfoString;     // YMM String of the current vehicle
+
+        // Public values for our view to bind onto 
+        public string SelectedDevice { get => _selectedDevice; set => PropertyUpdated(value); }
+        public double DeviceVoltage { get => _deviceVoltage; set => PropertyUpdated(value); }
+        public string VehicleVIN
+        {
+            get => _vehicleVIN ?? "No VIN Number"; 
+            set => PropertyUpdated(value);
+        }            
+        public string VehicleInfoString
+        {
+            get => _vehicleInfoString ?? "No VIN Number To Decode"; 
+            set => PropertyUpdated(value);
+        }
 
         // --------------------------------------------------------------------------------------------------------------------------
 
@@ -29,9 +53,33 @@ namespace FulcrumInjector.FulcrumViewContent.ViewModels
             // Log information and store values 
             ViewModelLogger.WriteLog($"VIEWMODEL LOGGER FOR VM {this.GetType().Name} HAS BEEN STARTED OK!", LogType.InfoLog);
             ViewModelLogger.WriteLog("SETTING UP HARDWARE INSTANCE VIEW BOUND VALUES NOW...", LogType.WarnLog);
+            
+            // Attach listeners to our device changed events.
+            ViewModelLogger.WriteLog("HOOKED NEW EVENT INSTANCE INTO OUR LISTENER FOR DEVICE CHANGED EVENTS OK!",  LogType.InfoLog);
+            this.SelectedDevice = InjectorConstants.FulcrumInstalledHardwareViewModel.SelectedDevice;
+            InjectorConstants.FulcrumInstalledHardwareViewModel.DeviceOrDllChanged += (Sender, Args) =>
+            {
+                // Build new listener object here.
+                ViewModelLogger.WriteLog("NEW DEVICE CHANGED EVENT PROCESSED!", LogType.InfoLog);
+                ViewModelLogger.WriteLog($"--> DEVICE NAME FOUND: {Args.DeviceName}");
+                ViewModelLogger.WriteLog($"--> DLL NAME FOUND:    {Args.DeviceDLL}");
+
+                // Store device and DLL info.
+                this.SelectedDevice = Args.DeviceName;
+                ViewModelLogger.WriteLog("STORED NEW DEVICE NAME OK!", LogType.InfoLog);
+            };
         }
 
         // --------------------------------------------------------------------------------------------------------------------------
 
+        /// <summary>
+        /// Updates our device voltage value based on our currently selected device information
+        /// </summary>
+        /// <returns>Voltage of the device connected and selected</returns>
+        public double RefreshDeviceVoltage() 
+        {
+            // Try and open the device to pull our voltage reading here
+            return 0.00;
+        }
     }
 }
