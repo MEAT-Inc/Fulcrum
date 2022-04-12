b93e13b1a83b413e9bdf4ad5e6ad63d9214d97f4
Zack Walsh <zachary.walsh@opusivs.com>
2022-04-12 12:22:56 -0400

    Fixed Style Sheet Locations
    Fixed issue with name lookup for style sheets

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("3.43.1.1119")]
-[assembly: AssemblyFileVersion("3.43.1.1119")]
+[assembly: AssemblyVersion("3.43.1.1121")]
+[assembly: AssemblyFileVersion("3.43.1.1121")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
