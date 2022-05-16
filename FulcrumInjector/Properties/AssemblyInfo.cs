9d018fd7709af1f19a0697cc30d4fdec837e1ed5
Zack Walsh <zachary.walsh@opusivs.com>
2022-05-16 17:02:01 -0400

    Built Tick for new changes

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("4.1.0.1367")]
-[assembly: AssemblyFileVersion("4.1.0.1367")]
+[assembly: AssemblyVersion("4.1.3.1368")]
+[assembly: AssemblyFileVersion("4.1.3.1368")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
