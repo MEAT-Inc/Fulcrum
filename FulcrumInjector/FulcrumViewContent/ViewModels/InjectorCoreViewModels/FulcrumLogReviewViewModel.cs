ceb7d8ecfd4cc12596c7e7c902b237bbe5852136
Zack Walsh <zachary.walsh@opusivs.com>
2022-06-09 11:29:34 -0400

    Building for newest SharpSimulator Package
    Updated some namespace issues that were being caused by changes to the Simulator package

--- FulcrumInjector/FulcrumViewContent/ViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
+++ FulcrumInjector/FulcrumViewContent/ViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
@@ -24,6 +24,7 @@ using SharpLogger;
 using SharpLogger.LoggerObjects;
 using SharpLogger.LoggerSupport;
 using SharpSimulator;
+using SharpSimulator.SimulationObjects;
 
 namespace FulcrumInjector.FulcrumViewContent.ViewModels.InjectorCoreViewModels
 {
