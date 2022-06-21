6d9c2267bd6696b36b5a35e2238314b39ea81949
Zack Walsh <zachary.walsh@opusivs.com>
2022-06-21 10:12:28 -0400

    Try catch routines around J Object conversions
    Added try/catches around the JObject conversion routines to stop instant failures on conversion runs.

--- FulcrumInjector/FulcrumLogic/PassThruLogic/PassThruSimulation/ExpressionToJ2534Object.cs
+++ FulcrumInjector/FulcrumLogic/PassThruLogic/PassThruSimulation/ExpressionToJ2534Object.cs
@@ -33,31 +33,42 @@ namespace FulcrumInjector.FulcrumLogic.PassThruLogic.PassThruSimulation
             }
 
             // Build filter output contents
-            var FilterFlags = uint.Parse(FilterContent[0][4]);
-            var FilterType = FilterExpression.FilterType.Split(':')[1];
-            var FilterProtocol = (ProtocolId)uint.Parse(FilterContent[0][2].Split(':')[0]);
-            var FilterPatten = FilterContent[0].Last().Replace("0x ", string.Empty);
-            var FilterMask = FilterContent[1].Last().Replace("0x ", string.Empty);
-            var FilterFlow = FilterContent.Count != 3 ? "" : FilterContent[2].Last().Replace("0x ", string.Empty);
-
-            // Now convert our information into string values.
-            return new J2534Filter()
+            try
             {
-                // Build a new filter object form the given values and return it.
-                FilterMask = FilterMask,
-                FilterFlags = FilterFlags,
-                FilterProtocol = FilterProtocol,
-                FilterPattern = Inverted ? FilterFlow : FilterPatten,
-                FilterFlowCtl = Inverted ? FilterPatten : FilterFlow,
-                FilterType = (FilterDef)Enum.Parse(typeof(FilterDef), FilterType)
-            };
+                var FilterFlags = uint.Parse(FilterContent[0].Last());
+                var FilterType = FilterExpression.FilterType.Split(':')[1];
+                var FilterProtocol = (ProtocolId)uint.Parse(FilterContent[0][2].Split(':')[0]);
+                var FilterPatten = FilterContent[0].Last().Replace("0x ", string.Empty);
+                var FilterMask = FilterContent[1].Last().Replace("0x ", string.Empty);
+                var FilterFlow = FilterContent.Count != 3 ? "" : FilterContent[2].Last().Replace("0x ", string.Empty);
+
+                // Now convert our information into string values.
+                return new J2534Filter()
+                {
+                    // Build a new filter object form the given values and return it.
+                    FilterMask = FilterMask,
+                    FilterFlags = FilterFlags,
+                    FilterProtocol = FilterProtocol,
+                    FilterPattern = Inverted ? FilterFlow : FilterPatten,
+                    FilterFlowCtl = Inverted ? FilterPatten : FilterFlow,
+                    FilterType = (FilterDef)Enum.Parse(typeof(FilterDef), FilterType)
+                };
+            }
+            catch (Exception ConversionEx)
+            {
+                // TODO: FIND OUT WHY THIS ROUTINE CAN FAIL SOMETIMES!
+                FilterExpression.ExpressionLogger.WriteLog("FAILED TO CONVERT THIS EXPRESSION INTO A USABLE FILTER! EXCEPTION IS BEING SHOWN BELOW", LogType.WarnLog);
+                FilterExpression.ExpressionLogger.WriteLog($"FILTER EXPRESSION: {FilterExpression.CommandLines}", LogType.TraceLog);
+                FilterExpression.ExpressionLogger.WriteLog("EXCEPTION THROWN", ConversionEx);
+                return null;
+            }
         }
         /// <summary>
         /// Converts a PTWrite Message Expression into a PTMessage
         /// </summary>
         /// <param name="MessageExpression"></param>
         /// <returns></returns>
-        public static PassThruStructs.PassThruMsg ConvertWriteExpression(PassThruWriteMessagesExpression MessageExpression)
+        public static PassThruStructs.PassThruMsg[] ConvertWriteExpression(PassThruWriteMessagesExpression MessageExpression)
         {
             // Store the Message Data and the values of the message params.
             MessageExpression.FindMessageContents(out List<string[]> MessageContents);
@@ -69,65 +80,77 @@ namespace FulcrumInjector.FulcrumLogic.PassThruLogic.PassThruSimulation
             }
 
             // Loop all the message values located and append them into the list of output
-            PassThruStructs.PassThruMsg MessageBuilt = default;
+            PassThruStructs.PassThruMsg[] MessagesBuilt = Array.Empty<PassThruStructs.PassThruMsg>();
             foreach (var MessageSet in MessageContents)
             {
-                // Store message values here.
-                var MessageData = MessageSet.Last();
-                var MessageFlags = uint.Parse(MessageSet[3]);
-                var ProtocolId = (ProtocolId)Enum.Parse(typeof(ProtocolId), MessageSet[4].Split(':')[0]);
-
-                // TODO: FORMAT THIS CODE TO WORK FOR DIFFERENT PROTOCOL VALUE TYPES!
-
-                // ISO15765 11 Bit
-                if (MessageData.StartsWith("00 00"))      
+                // Wrap inside a try catch to ensure we get something back
+                try
                 {
-                    // 11 Bit messages need to be converted according to this format
-                    // 00 00 07 DF 01 00 -- 00 00 07 DF 02 01 00 00 00 00 00 00
-                    // Take first 4 bytes 
-                    //      00 00 07 DF
-                    // Count the number of bytes left
-                    //      01 00 -- 2 bytes
-                    // Insert the number of bytes (02) and the data sent
-                    //      00 00 07 DF 02 01 00
-                    // Take the number of bytes now and append 0s till it's 12 long
-                    //      00 00 07 DF 02 01 00 00 00 00 00 00
+                    // Store message values here.
+                    var MessageData = MessageSet.Last();
+                    var MessageFlags = uint.Parse(MessageSet[3]);
+                    var ProtocolId = (ProtocolId)Enum.Parse(typeof(ProtocolId), MessageSet[4].Split(':')[0]);
 
-                    // Build a message and then return it.
-                    string[] MessageDataSplit = MessageData.Split(' ').ToArray();
-                    string[] FormattedData = MessageDataSplit.Take(4).ToArray();                           // 00 00 07 DF
-                    string[] DataSentOnly = MessageDataSplit.Skip(4).ToArray();                            // 01 00
-                    string[] FinalData = FormattedData
-                        .Concat(new string[] { "0x" + DataSentOnly.Length.ToString("X") })
-                        .Concat(DataSentOnly)
-                        .ToArray();                                                                        // 00 00 07 DF 02 01 00                                                           
-                    string[] TrailingZeros = Enumerable.Repeat("0x00", 12 - FinalData.Length).ToArray();
-                    FinalData = FinalData.Concat(TrailingZeros).ToArray();                                 // 00 00 07 DF 02 01 00 00 00 00 00
+                    // TODO: FORMAT THIS CODE TO WORK FOR DIFFERENT PROTOCOL VALUE TYPES!
 
-                    // Convert back into a string value and format
-                    MessageData = string.Join(" ", FinalData);
-                    MessageData = MessageData.Replace("0x", string.Empty);
+                    // ISO15765 11 Bit
+                    if (MessageData.StartsWith("00 00"))
+                    {
+                        // 11 Bit messages need to be converted according to this format
+                        // 00 00 07 DF 01 00 -- 00 00 07 DF 02 01 00 00 00 00 00 00
+                        // Take first 4 bytes 
+                        //      00 00 07 DF
+                        // Count the number of bytes left
+                        //      01 00 -- 2 bytes
+                        // Insert the number of bytes (02) and the data sent
+                        //      00 00 07 DF 02 01 00
+                        // Take the number of bytes now and append 0s till it's 12 long
+                        //      00 00 07 DF 02 01 00 00 00 00 00 00
+
+                        // Build a message and then return it.
+                        string[] MessageDataSplit = MessageData.Split(' ').ToArray();
+                        string[] FormattedData = MessageDataSplit.Take(4).ToArray();                           // 00 00 07 DF   --   Padding + Send Address
+                        string[] DataSentOnly = MessageDataSplit.Skip(4).ToArray();                            // 01 00         --   Actual data transmission
+                        string[] FinalData = FormattedData
+                            .Concat(new string[] { "0x" + DataSentOnly.Length.ToString("X") })
+                            .Concat(DataSentOnly)
+                            .ToArray();                                                                        // 00 00 07 DF 02 01 00   --   Padding + Send Addr + Size + Data                                                         
+                        string[] TrailingZeros = Enumerable.Repeat("0x00", 12 - FinalData.Length).ToArray();
+                        FinalData = FinalData.Concat(TrailingZeros).ToArray();                                 // 00 00 07 DF 02 01 00 00 00 00 00 -- Finalized output message
+
+                        // Convert back into a string value and format
+                        MessageData = string.Join(" ", FinalData);
+                        MessageData = MessageData.Replace("0x", string.Empty);
+                    }
+
+                    // ISO15765 29 Bit
+                    if (MessageData.StartsWith("18 db")) {
+                        // TODO: BUILD FORMATTING ROUTINE FOR 29 BIT CAN!
+                    }
+
+                    // Build our final output message.
+                    var NextMessage = J2534Device.CreatePTMsgFromString(ProtocolId, MessageFlags, MessageData);
+                    MessagesBuilt = MessagesBuilt.Append(NextMessage).ToArray();
                 }
-
-                // ISO15765 29 Bit
-                if (MessageData.StartsWith("18 db"))
+                catch (Exception ConversionEx)
                 {
-                    // TODO: BUILD FORMATTING ROUTINE FOR 29 BIT CAN!
+                    // TODO: FIND OUT WHY THIS ROUTINE CAN FAIL SOMETIMES!
+                    MessageExpression.ExpressionLogger.WriteLog("FAILED TO CONVERT THIS EXPRESSION INTO A USABLE MESSAGE! EXCEPTION IS BEING SHOWN BELOW", LogType.WarnLog);
+                    MessageExpression.ExpressionLogger.WriteLog($"MESSAGE EXPRESSION: {MessageExpression.CommandLines}", LogType.TraceLog);
+                    MessageExpression.ExpressionLogger.WriteLog("EXCEPTION THROWN", ConversionEx);
+                    return default;
                 }
-
-                // Build our final output message.
-                MessageBuilt = J2534Device.CreatePTMsgFromString(ProtocolId, MessageFlags, MessageData);
             }
 
             // Return the built message
-            return MessageBuilt;
+            return MessagesBuilt;
         }
         /// <summary>
         /// Converts an input PTRead Expression to a PTMessage
         /// </summary>
         /// <param name="MessageExpression"></param>
         /// <returns></returns>
-        public static PassThruStructs.PassThruMsg ConvertReadExpression(PassThruReadMessagesExpression MessageExpression)
+        public static PassThruStructs.PassThruMsg[] ConvertReadExpression(PassThruReadMessagesExpression MessageExpression)
         {
             // Store the Message Data and the values of the message params.
             MessageExpression.FindMessageContents(out List<string[]> MessageContents);
@@ -139,36 +162,49 @@ namespace FulcrumInjector.FulcrumLogic.PassThruLogic.PassThruSimulation
             }
 
             // Loop all the message values located and append them into the list of output
-            PassThruStructs.PassThruMsg BuiltMessage = default;
+            PassThruStructs.PassThruMsg[] MessagesBuilt = Array.Empty<PassThruStructs.PassThruMsg>();
             foreach (var MessageSet in MessageContents)
             {
-                // Store message values here.
-                var MessageData = MessageSet.Last();
-                if (MessageData.Contains("[") || MessageData.Contains("]"))
+                // Wrap this in a try catch
+                try
                 {
-                    // Format for framepad output
+                    // Store message values here.
+                    var MessageData = MessageSet.Last();
+                    if (MessageData.Contains("[") || MessageData.Contains("]"))
+                    {
+                        // Format for framepad output
+                        MessageData = MessageData.Replace("0x", string.Empty);
+                        string[] SplitData = MessageData
+                            .Split(']')
+                            .Select(SplitPart => SplitPart.Replace("[", string.Empty))
+                            .Where(SplitPart => SplitPart.Length != 0)
+                            .ToArray();
+
+                        // Now restore message values
+                        MessageData = string.Join(" ", SplitData);
+                    }
+
+                    // If it's not a frame pad message, add to our simulation
+                    var MessageFlags = uint.Parse(MessageSet[4]);
+                    var ProtocolId = (ProtocolId)Enum.Parse(typeof(ProtocolId), MessageSet[2].Split(':')[0]);
+
+                    // Build a message and then return it.
                     MessageData = MessageData.Replace("0x", string.Empty);
-                    string[] SplitData = MessageData
-                        .Split(']')
-                        .Select(SplitPart => SplitPart.Replace("[", string.Empty))
-                        .Where(SplitPart => SplitPart.Length != 0)
-                        .ToArray();
-
-                    // Now restore message values
-                    MessageData = string.Join(" ", SplitData);
+                    var NextMessage = J2534Device.CreatePTMsgFromString(ProtocolId, MessageFlags, MessageData);
+                    MessagesBuilt = MessagesBuilt.Append(NextMessage).ToArray();
                 }
-
-                // If it's not a frame pad message, add to our simulation
-                var MessageFlags = uint.Parse(MessageSet[4]);
-                var ProtocolId = (ProtocolId)Enum.Parse(typeof(ProtocolId), MessageSet[2].Split(':')[0]);
-
-                // Build a message and then return it.
-                MessageData = MessageData.Replace("0x", string.Empty);
-                BuiltMessage = J2534Device.CreatePTMsgFromString(ProtocolId, MessageFlags, MessageData);
+                catch (Exception ConversionEx)
+                {
+                    // TODO: FIND OUT WHY THIS ROUTINE CAN FAIL SOMETIMES!
+                    MessageExpression.ExpressionLogger.WriteLog("FAILED TO CONVERT THIS EXPRESSION INTO A USABLE MESSAGE! EXCEPTION IS BEING SHOWN BELOW", LogType.WarnLog);
+                    MessageExpression.ExpressionLogger.WriteLog($"MESSAGE EXPRESSION: {MessageExpression.CommandLines}", LogType.TraceLog);
+                    MessageExpression.ExpressionLogger.WriteLog("EXCEPTION THROWN", ConversionEx);
+                    return default;
+                }
             }
 
             // Return the message
-            return BuiltMessage;
+            return MessagesBuilt;
         }
     }
 }
