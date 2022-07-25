5f55f1003c53ee8ab47b77d423b3c8c007b706e0
Zack Walsh <zachary.walsh@opusivs.com>
2022-07-25 09:18:00 -0400

    Updating nuget packages

---
+++ FulcrumInjector/FulcrumLogic/FulcrumUpdater/InjectorInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulcrumInjector.FulcrumLogic.FulcrumUpdater
+{
+    /// <summary>
+    /// Class object housing the logic used for installing a newly downloaded injector MSI file
+    /// </summary>
+    public class InjectorInstaller
+    {
+
+    }
+}
---
+++ FulcrumInjector/FulcrumLogic/FulcrumUpdater/InjectorUpdater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulcrumInjector.FulcrumLogic.FulcrumUpdater
+{
+    /// <summary>
+    /// Class which houses the logic for pulling in a new Fulcrum Injector MSI File.
+    /// </summary>
+    public class InjectorUpdater
+    {
+        // Values used to setup getting a list of all MSI Files
+
+    }
+}
