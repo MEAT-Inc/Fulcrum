085df5ab314b99804e68aa891cd07898a3f42307
Zack Walsh <zachary.walsh@opusivs.com>
2022-07-25 10:38:01 -0400

    Removed OctoKit and updated

---
+++ FulcrumInjector/FulcrumLogic/FulcrumUpdater/InjectorUpdater.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FulcrumInjector.FulcrumLogic.JsonLogic.JsonHelpers;
+using SharpLogger;
+using SharpLogger.LoggerObjects;
+using SharpLogger.LoggerSupport;
+
+namespace FulcrumInjector.FulcrumLogic.FulcrumUpdater
+{
+    /// <summary>
+    /// Class which houses the logic for pulling in a new Fulcrum Injector MSI File.
+    /// </summary>
+    public class InjectorUpdater
+    {
+        // Logger object.
+        private SubServiceLogger _injectorUpdateLogger => (SubServiceLogger)LogBroker.LoggerQueue.GetLoggers(LoggerActions.SubServiceLogger)
+            .FirstOrDefault(LoggerObj => LoggerObj.LoggerName.StartsWith("InjectorUpdateLogger")) ?? new SubServiceLogger("InjectorUpdateLogger");
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+
+        // Values used to setup getting a list of all MSI Files
+
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Builds a new injector update helper object which pulls our GitHub release information
+        /// </summary>
+        public InjectorUpdater()
+        {
+
+        }
+    }
+}
