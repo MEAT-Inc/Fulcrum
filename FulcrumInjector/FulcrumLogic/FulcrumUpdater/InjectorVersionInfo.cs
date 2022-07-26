b14e1ee965da44e48ad5bb27bf5e56f600309ab2
Zack Walsh <zachary.walsh@opusivs.com>
2022-07-26 17:10:50 -0400

    Built installer for updater version
    Version 5.2.0.1756 is built.

--- FulcrumInjector/FulcrumLogic/FulcrumUpdater/InjectorVersionInfo.cs
+++ FulcrumInjector/FulcrumLogic/FulcrumUpdater/InjectorVersionInfo.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using FulcrumInjector.FulcrumLogic.JsonLogic.JsonHelpers;
 
 namespace FulcrumInjector.FulcrumLogic.FulcrumUpdater
 {
