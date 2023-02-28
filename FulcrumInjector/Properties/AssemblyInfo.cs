318dc7845476eec29fe30351a1a7b41d771fa12f
Zack Walsh <zack.walsh@meatinc.autos>
2023-02-28 12:00:17 -0500

    Fixed an issue in the Assembly info file

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -1,1 +1,25 @@
-﻿ErrorGeneratingOutput
+﻿// Using calls for Assembly Info updating
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("FulcrumInjector")]
+[assembly: AssemblyDescription("The Injector is an app which boots an OE Software (or any diag software for that matter), shims it, and sniffs the shit outta it")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MEAT Inc")]
+[assembly: AssemblyProduct("FulcrumInjector")]
+[assembly: AssemblyCopyright("Copyright ©MEAT Inc 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Internal Visibility for testing
+[assembly: ComVisible(false)]
+[assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
+
+// Version information
+[assembly: AssemblyVersion("6.3.0.2265")]
+[assembly: AssemblyFileVersion("6.3.0.2265")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
