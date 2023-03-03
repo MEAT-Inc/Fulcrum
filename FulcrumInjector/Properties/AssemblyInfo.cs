33828791b285e2b260523a5e4be27b44db91d989
Zack Walsh <zack.walsh@meatinc.autos>
2023-03-03 11:34:13 -0500

    Package upgrades

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("6.3.10.2328")]
-[assembly: AssemblyFileVersion("6.3.10.2328")]
+[assembly: AssemblyVersion("6.3.10.2329")]
+[assembly: AssemblyFileVersion("6.3.10.2329")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
