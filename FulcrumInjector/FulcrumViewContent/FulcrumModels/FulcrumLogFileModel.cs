c0ad0a348699c92893e1ad9723c87e4f3c1698b4
Zack Walsh <zack.walsh@meatinc.autos>
2023-03-07 09:00:51 -0500

    Added new model object for injector log files
    Added new log file model which holds information about the different log file types

---
+++ FulcrumInjector/FulcrumViewContent/FulcrumModels/FulcrumLogFileModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FulcrumInjector.FulcrumViewContent.FulcrumModels
+{
+    /// <summary>
+    /// Internal class holding the values for our different types of log files supported
+    /// </summary>
+    internal class FulcrumLogFileModel
+    {
+        #region Custom Events
+        #endregion //Custom Events
+
+        #region Fields
+        #endregion //Fields
+
+        #region Properties
+        #endregion //Properties
+
+        #region Structs and Classes
+        #endregion //Structs and Classes
+    }
+}
