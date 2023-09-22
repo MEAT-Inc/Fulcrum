cdba20209e62219f4743a2979247a48b20a46715
Zack Walsh <zachary.walsh@opusivs.com>
2023-09-22 15:27:01 -0400

    Building release version 6.8.5.2789

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("6.8.5.2789")]
-[assembly: AssemblyFileVersion("6.8.5.2789")]
+[assembly: AssemblyVersion("6.8.5.2790")]
+[assembly: AssemblyFileVersion("6.8.5.2790")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
