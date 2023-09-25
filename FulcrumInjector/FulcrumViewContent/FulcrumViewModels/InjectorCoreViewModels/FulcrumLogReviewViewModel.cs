a24e513eaf6b788a0fc89ce076d247d07bdf5788
Zack Walsh <zack.walsh@meatinc.autos>
2023-09-25 12:43:34 -0400

    Updated sim generator to be view model object

--- FulcrumInjector/FulcrumViewContent/FulcrumViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
+++ FulcrumInjector/FulcrumViewContent/FulcrumViewModels/InjectorCoreViewModels/FulcrumLogReviewViewModel.cs
@@ -49,7 +49,7 @@ namespace FulcrumInjector.FulcrumViewContent.FulcrumViewModels.InjectorCoreViewM
 
         // Private backing fields for an expressions and simulations generator
         private PassThruExpressionsGenerator _expGenerator;         // The expressions generator for this view model
-        private PassThruExpressionsGenerator _simGenerator;         // The simulation generator for this view model
+        private PassThruSimulationGenerator _simGenerator;          // The simulation generator for this view model
         
         #endregion // Fields
 
@@ -251,8 +251,8 @@ namespace FulcrumInjector.FulcrumViewContent.FulcrumViewModels.InjectorCoreViewM
                 // Log we're building a simulation file set and build a new expressions generator here 
                 this.ProcessingProgress = 0;
                 this.ViewModelLogger.WriteLog("BUILDING SIMULATION FROM LOADED LOG FILE NOW...", LogType.InfoLog);
-                var SimGenerator = new PassThruSimulationGenerator(this.CurrentLogSet.PassThruLogFile.LogFilePath, this.CurrentLogSet.GeneratedExpressions);
-                SimGenerator.OnGeneratorProgress += (_, GeneratorArgs) =>
+                this._simGenerator = new PassThruSimulationGenerator(this.CurrentLogSet.PassThruLogFile.LogFilePath, this.CurrentLogSet.GeneratedExpressions);
+                this._simGenerator.OnGeneratorProgress += (_, GeneratorArgs) =>
                 {
                     // If the progress value reported back is the same as it is currently, don't set it again
                     int NextProgress = (int)GeneratorArgs.CurrentProgress;
@@ -262,12 +262,12 @@ namespace FulcrumInjector.FulcrumViewContent.FulcrumViewModels.InjectorCoreViewM
                 // Get our debug configuration value for enabling generator debugging
                 var ConversionSettings = FulcrumConstants.FulcrumSettings[FulcrumSettingsCollection.SettingSectionTypes.LOG_FILE_CONVERSION_SETTINGS];
                 var EnableGeneratorLogging = ConversionSettings.GetSettingValue("Debug Simulations Generator", false);
-                this.ViewModelLogger.WriteLog($"EXPRESSIONS GENERATOR DEBUG LOGGING IS SET TO: {EnableGeneratorLogging}");
+                this.ViewModelLogger.WriteLog($"SIMULATOR GENERATOR DEBUG LOGGING IS SET TO: {EnableGeneratorLogging}");
 
                 // Now Build our simulation content objects for this generator
                 this.ViewModelLogger.WriteLog("PROCESSING LOG LINES INTO SIM CHANNEL OBJECTS NOW...", LogType.InfoLog);
-                var BuiltSimChannels = SimGenerator.GenerateLogSimulation(EnableGeneratorLogging);
-                var BuiltSimFileName = SimGenerator.SaveSimulationFile(this.CurrentLogSet.PassThruLogFile.LogFilePath);
+                var BuiltSimChannels = this._simGenerator.GenerateLogSimulation(EnableGeneratorLogging);
+                var BuiltSimFileName = this._simGenerator.SaveSimulationFile(this.CurrentLogSet.PassThruLogFile.LogFilePath);
                 if (BuiltSimFileName == "") throw new InvalidOperationException("FAILED TO FIND OUT NEW SIMULATION CONTENT!");
 
                 // Once we've built the new simulations file contents and files, we can store them on our log file set
