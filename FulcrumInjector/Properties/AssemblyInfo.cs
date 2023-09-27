bf95a2ebabf2d6b1d3baddadc11ea0c1d39af665
Zack Walsh <zack.walsh@meatinc.autos>
2023-09-27 13:19:43 -0400

    Build tick and filter controls updated
    Build tick and added edit controls for filters

--- FulcrumInjector/Properties/AssemblyInfo.cs
+++ FulcrumInjector/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("8cb7e832-9e90-4820-b225-0a4d59e6c0a2")]
 
 // Version information
-[assembly: AssemblyVersion("6.9.2.2856")]
-[assembly: AssemblyFileVersion("6.9.2.2856")]
+[assembly: AssemblyVersion("6.9.3.2857")]
+[assembly: AssemblyFileVersion("6.9.3.2857")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
