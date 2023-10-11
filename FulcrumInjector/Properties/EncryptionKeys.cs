a839b2efb3cbb673422f66a52a9500418f0f84a1
Zack Walsh <zachary.walsh@opusivs.com>
2023-10-11 09:48:08 -0400

    Built some basic logic for encryption key class

--- FulcrumInjector/Properties/EncryptionKeys.cs
+++ FulcrumInjector/Properties/EncryptionKeys.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpLogging;
 
 namespace FulcrumInjector.Properties
 {
@@ -12,12 +13,15 @@ namespace FulcrumInjector.Properties
     ///       You can get these keys by reaching out to zack.walsh@meatinc.autos. This was done to we can store secrets in the AppSettings.json
     ///       file without risking leaving their contents exposed to the public. Thanks for understanding.
     /// </summary>
-    internal static class EncryptionKeys
+    public static class EncryptionKeys
     {
         #region Custom Events
         #endregion // Custom Events
 
         #region Fields
+
+        // Private static logger object for the encryption keys class
+        private static readonly SharpLogger _encryptionKeyLogger;
 
         // Static readonly fields for byte content/encryption
         public static readonly byte[] AuthKey = new byte[] { };
@@ -26,9 +30,25 @@ namespace FulcrumInjector.Properties
         #endregion // Fields
 
         #region Properties
+        
+        // Public static properties with helpful configuration information 
+        public static bool EncryptionConfigured => CryptoKey.Length > 0 && AuthKey.Length > 0;
+
         #endregion // Properties
 
         #region Structs and Classes
         #endregion // Structs and Classes
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Static CTOR for the encryption keys class
+        /// </summary>
+        static EncryptionKeys()
+        {
+            // Configure our logger instance and exit out
+            _encryptionKeyLogger = new SharpLogger(LoggerActions.UniversalLogger, "EncryptionKeysLogger");
+            _encryptionKeyLogger.WriteLog("ENCRYPTION KEY LOGGER HAS BEEN CONFIGURED!", LogType.InfoLog);
+        } 
     }
 }
