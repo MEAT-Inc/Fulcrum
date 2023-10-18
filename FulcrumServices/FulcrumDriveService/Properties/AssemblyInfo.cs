d9c722420fd9f4cd2475204f9c092c3ad4770869
Zack Walsh <zachary.walsh@opusivs.com>
2023-10-18 12:59:52 -0400

    Build testing

--- FulcrumServices/FulcrumDriveService/Properties/AssemblyInfo.cs
+++ FulcrumServices/FulcrumDriveService/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("dfd619be-ce63-4e4d-9c23-17016793244c")]
 
 // Version information
-[assembly: AssemblyVersion("0.1.3.23")]
-[assembly: AssemblyFileVersion("0.1.3.23")]
+[assembly: AssemblyVersion("0.1.3.24")]
+[assembly: AssemblyFileVersion("0.1.3.24")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
