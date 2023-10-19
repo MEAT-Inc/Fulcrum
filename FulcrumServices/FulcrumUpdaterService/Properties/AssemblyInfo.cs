e48462c3a958ec2ef4cb1c7843ef694ab5ed958e
Zack Walsh <zack.walsh@meatinc.autos>
2023-10-19 11:50:18 -0400

    Build testing

--- FulcrumServices/FulcrumUpdaterService/Properties/AssemblyInfo.cs
+++ FulcrumServices/FulcrumUpdaterService/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("5577927f-40b8-4ecb-b168-1cc06bd7b8f8")]
 
 // Version information
-[assembly: AssemblyVersion("0.1.2.30")]
-[assembly: AssemblyFileVersion("0.1.2.30")]
+[assembly: AssemblyVersion("0.1.2.31")]
+[assembly: AssemblyFileVersion("0.1.2.31")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
--- FulcrumSupport/FulcrumJson/Properties/AssemblyInfo.cs
+++ FulcrumSupport/FulcrumJson/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("9abe053a-d9e7-4bf0-aab9-e526e3430b88")]
 
 // Version information
-[assembly: AssemblyVersion("0.1.1.10")]
-[assembly: AssemblyFileVersion("0.1.1.10")]
+[assembly: AssemblyVersion("0.1.1.11")]
+[assembly: AssemblyFileVersion("0.1.1.11")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
