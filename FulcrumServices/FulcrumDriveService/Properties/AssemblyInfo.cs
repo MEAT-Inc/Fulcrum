c96a5252fdcb089f29af68363f93b4d40bd880d4
Zack Walsh <zachary.walsh@opusivs.com>
2023-10-24 10:26:13 -0400

    Updated services to install with MSI
    MSI now installs services when invoked to install the injector application

--- FulcrumServices/FulcrumDriveService/Properties/AssemblyInfo.cs
+++ FulcrumServices/FulcrumDriveService/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("dfd619be-ce63-4e4d-9c23-17016793244c")]
 
 // Version information
-[assembly: AssemblyVersion("0.1.3.30")]
-[assembly: AssemblyFileVersion("0.1.3.30")]
+[assembly: AssemblyVersion("0.1.3.31")]
+[assembly: AssemblyFileVersion("0.1.3.31")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
